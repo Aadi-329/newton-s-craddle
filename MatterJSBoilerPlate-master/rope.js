64a2b45d6f5180bbc21a04af46de3e4cd0b21b8c
aadityasalecha <67157010+aadityasalecha@users.noreply.github.com>
2020-07-12 18:42:16 +0530

    Update rope.js

--- MatterJSBoilerPlate-master/rope.js
+++ MatterJSBoilerPlate-master/rope.js
@@ -1,18 +1,27 @@
+var options
 class Rope{
-    constructor(bodyA, bodyB){
-        var options = {
-            bodyA: bodyA,
-            bodyB: bodyB,
-            stiffness: 0.04,
-            length: 25
-        }
-      this.rope=Constraint.create(options);
-        World.add(world, this.rope);
+    constructor(body1,body2,offsetX,offsetY){
+        this.offsetX=offsetX
+   this.offsetY=offsetY
+         options = {
+            bodyA: body1,
+            bodyB: body2,
+            pointB:{x:this.offsetX,y:this.offsetY},
+
+    }
+   
+      this.rope=Constraint.create(options)
+        World.add(world,this.rope)
     }
 
 
     display(){
-              line(bodyA.x, bodyA.y, bodyB.x, bodyB.y);
+var pointc=this.rope.BodyA.position;
+var pointd=this.rope.bodyB.position;
+
+StrokeWeight(2);
+
+             line(pointc.x,pointc.y,pointd.x+this.offsetX,pointd.y+this.offsetY);
        
 
 
